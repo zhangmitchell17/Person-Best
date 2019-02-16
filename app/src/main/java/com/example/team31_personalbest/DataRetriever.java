8584d9bbd786d97252eb86fe02ffa64793a40ca5
David Nguyen <davidhungng@gmail.com>
2019-02-15 22:44:54 -0800

    Issue #50: More template code for data retrieval via Google Fit History API

--- app/src/main/java/com/example/team31_personalbest/DataRetriever.java
+++ app/src/main/java/com/example/team31_personalbest/DataRetriever.java
@@ -1,4 +1,184 @@
 package com.example.team31_personalbest;
 
-public class DataRetriever {
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptionsExtension;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.Bucket;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.result.DataReadResult;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+public class DataRetriever implements FitnessService {
+    private final int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = System.identityHashCode(this) & 0xFFFF;
+    private final String TAG = "DataRetriever";
+
+    private Activity activity;
+    private GoogleApiClient historyClient;
+
+    public DataRetriever(Activity activity) {
+        this.activity = activity;
+    }
+
+
+    /**
+     * filling implementation from FitnessService
+     */
+    public void setup() {
+        historyClient = new GoogleApiClient.Builder(activity)
+                .addApi(Fitness.HISTORY_API)
+                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
+                .addConnectionCallbacks((GoogleApiClient.ConnectionCallbacks)activity)
+                .enableAutoManage((FragmentActivity) activity, 0, (GoogleApiClient.OnConnectionFailedListener) activity)
+                .build();
+
+        /*
+            if permissions aren't yet granted, request for them, otherwise continue as normal
+         */
+        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(activity),
+                                        (GoogleSignInOptionsExtension)historyClient)) {
+            GoogleSignIn.requestPermissions(
+                    activity, // your activity
+                    GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
+                    GoogleSignIn.getLastSignedInAccount(activity),
+                    (GoogleSignInOptionsExtension)historyClient);
+        } else {
+            updateStepCount();
+            subscribe();
+        }
+    }
+
+    private void subscribe() {
+        //check if logged in
+        GoogleSignInAccount lastSignedInAccount = GoogleSignIn.getLastSignedInAccount(activity);
+        if (lastSignedInAccount == null) {
+            return;
+        }
+
+        /* subscribing to total steps, */
+        Fitness.getRecordingClient(activity, GoogleSignIn.getLastSignedInAccount(activity))
+                .subscribe(DataType.TYPE_STEP_COUNT_CUMULATIVE)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.i(TAG, "Successfully subscribed!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i(TAG, "There was a problem subscribing.");
+                    }
+                });
+
+    }
+
+
+    /**
+     * Necessary to implement FitnessService
+     */
+    public void updateStepCount() {
+    }
+
+
+    /**
+     * filling implementation from FitnessService
+     */
+    @Override
+    public int getRequestCode() {
+        return GOOGLE_FIT_PERMISSIONS_REQUEST_CODE;
+    }
+
+
+    // should request different types of data
+    //      for the progressChart we should get steps and planned steps per each day
+    //      for other data we should get activity steps, time, and speed
+    public void retrieveData() {
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        long startTime = cal.getTimeInMillis();
+
+        java.text.DateFormat dateFormat = DateFormat.getDateInstance();
+        Log.e("History", "Range Start: " + dateFormat.format(startTime));
+        Log.e("History", "Range End: " + dateFormat.format(endTime));
+
+        /*
+        Requests for regular steps and planned steps respectively
+         */
+        //Check how many steps were walked and recorded in the last 7 days
+        DataReadRequest regStepRequest = new DataReadRequest.Builder()
+                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                .bucketByTime(1, TimeUnit.DAYS)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                .build();
+
+        //Check how many steps were walked and recorded in the last 7 days
+        DataReadRequest plannedStepRequest = new DataReadRequest.Builder()
+                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                .bucketByTime(1, TimeUnit.DAYS)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                .build();
+
+        DataReadResult dataReadResult = Fitness.HistoryApi.readData(historyClient, regStepRequest)
+                .await(1, TimeUnit.MINUTES);
+        //Used for aggregated data
+        if (dataReadResult.getBuckets().size() > 0) {
+            Log.e("History", "Number of buckets: " + dataReadResult.getBuckets().size());
+            for (Bucket bucket : dataReadResult.getBuckets()) {
+                List<DataSet> dataSets = bucket.getDataSets();
+                for (DataSet dataSet : dataSets) {
+                    showDataSet(dataSet);
+                }
+            }
+        }
+        //Used for non-aggregated data
+        else if (dataReadResult.getDataSets().size() > 0) {
+            Log.e("History", "Number of returned DataSets: " + dataReadResult.getDataSets().size());
+            for (DataSet dataSet : dataReadResult.getDataSets()) {
+                showDataSet(dataSet);
+
+            }
+        }
+    }
+    private void showDataSet(DataSet dataSet) {
+        Log.e("History", "Data returned for Data type: " + dataSet.getDataType().getName());
+        DateFormat dateFormat = DateFormat.getDateInstance();
+        DateFormat timeFormat = DateFormat.getTimeInstance();
+
+        for (DataPoint dp : dataSet.getDataPoints()) {
+            Log.e("History", "Data point:");
+            Log.e("History", "\tType: " + dp.getDataType().getName());
+            Log.e("History", "\tStart: " + dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)) + " " + timeFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+            Log.e("History", "\tEnd: " + dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)) + " " + timeFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+            for(Field field : dp.getDataType().getFields()) {
+                Log.e("History", "\tField: " + field.getName() +
+                        " Value: " + dp.getValue(field));
+            }
+        }
+    }
 }
