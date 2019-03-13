3ed6dd71ca0b9e35279dd38c2a6d509c399ec17f
HongyuZou <hoz054@ucsd.edu>
2019-03-12 21:40:52 -0700

    #6 add user in main activity

--- app/src/main/java/com/example/team31_personalbest_ms2v2/MainActivity.java
+++ app/src/main/java/com/example/team31_personalbest_ms2v2/MainActivity.java
@@ -84,6 +84,7 @@ public class MainActivity extends AppCompatActivity
 
     public String currentUserEmail = "test@ucsd.edu";
     public String currentUserName = "test";
+    User user;
     public FirebaseFirestore db;
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
@@ -457,6 +458,14 @@ public class MainActivity extends AppCompatActivity
     }
 
     /**
+     * Add a user to the database
+     * @param user name of the user
+     */
+    public void addUser(User user) {
+        db.collection("users").document(user.email).set(user);
+    }
+
+    /**
      * Override the onActivityResult method from super class
      * handle the sign in result of the task
      * @param requestCode
@@ -472,6 +481,8 @@ public class MainActivity extends AppCompatActivity
         if (acct != null) {
             this.currentUserEmail = acct.getEmail();
             this.currentUserName = acct.getDisplayName();
+            this.user = new User(currentUserName, currentUserEmail);
+            addUser(user);
             launchInputHeightStepGoalActivity();
         }
 
