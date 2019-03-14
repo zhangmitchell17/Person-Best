d221e12c2e870c9955010920aacdccbc830fa9cf
David Nguyen <davidhungng@gmail.com>
2019-03-13 21:24:37 -0700

    #56: commenting

--- app/src/main/java/com/example/team31_personalbest_ms2v2/IDataRetrieverObserver.java
+++ app/src/main/java/com/example/team31_personalbest_ms2v2/IDataRetrieverObserver.java
@@ -2,6 +2,10 @@ package com.example.team31_personalbest_ms2v2;
 
 import java.util.List;
 
+/**
+ * interface that allows observers to update themselves
+ * when data has been retrieved by the subject they are observing
+ */
 public interface IDataRetrieverObserver {
     void onDataRetrieved(String label, List<Integer> list);
 }
